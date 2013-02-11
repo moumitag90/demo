7366dfa15491c8d1e4f0d28a1b9b5f13cf2e7abf
moumitag90 <moumita@ubuntu.ubuntu-domain>
2013-02-11 11:48:47 +0530

    pushed

--- AppMou/app/controllers/posts_controller.rb
+++ AppMou/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
 def new
 @post=Post.new
 end
@@ -8,29 +9,36 @@ user=User.find(params[:user_id])
 post=user.posts.create(params[:post])
 redirect_to user_path(user)
 end
+
 def show
-  user =User.find(params[:id])
-  @posts=user.posts.find(:all)
-  
+@user = User.find(params[:user_id])
+@post= @user.posts.find(params[:id])
 end
 
-def edit 
- user=User.find(params[:id])
- @posts=user.posts.find(params[:user_id])
+def index
+@user =User.find(params[:user_id])
+@posts=@user.posts.find(:all)
+
+end
+
+def edit
+@user=User.find(params[:user_id])
+@post=@user.posts.find(params[:id])
 end
 
 def update
-  user=User.find(params[:id])
-  @post=user.posts.find(params[:user_id])
-  @post.update_attributes(params[:post])
-  @post.save!
-  redirect_to user_post_path(user)
- end
+@user=User.find(params[:user_id])
+@post=@user.posts.find(params[:id])
+@post.update_attributes(params[:post])
+@post.save!
+redirect_to user_post_path(@user)
+end
 
 def destroy
- user=User.find(params[:id])
- post=user.posts.find(params[:user_id])
- post.destroy
- redirect_to user_post_path(user)
+@user = User.find(params[:user_id])
+@post= @user.posts.find(params[:id])
+@post.destroy
+redirect_to user_posts_path(@user)
 end
+
 end
