b8f2f67df81cdbe27b8627c7744d654b0b910b1d
moumitag90 <moumita@ubuntu.ubuntu-domain>
2013-02-13 21:51:34 +0530

    deleted

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Demo::Application.config.secret_token = '6a31eaf7bf0a40313795416aa404b10a9c3b395a0c3d6b2935a37ec9acf431b4b8f91339a5081186d8ddda838b278b77c395000b474f1234050c2f3f598e0052'
